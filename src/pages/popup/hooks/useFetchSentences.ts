994230558d04bdb19ef2516548db8e0578b5bcda
mshtang <32868278+mshtang@users.noreply.github.com>
2024-01-08 18:51:23 +0100

    refactor: add .env file and use mock api response for development

--- src/pages/popup/hooks/useFetchSentences.ts
+++ src/pages/popup/hooks/useFetchSentences.ts
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import mockApiResponse from '../mocks/mockApiResponse.json';
 import { ApiResponseForSentence } from '../types/ApiResponseInterfaces';
 
 const useFetchSentences = (selectedCorpus: string, query: string, itemsToShow: number) => {
@@ -7,13 +8,22 @@ const useFetchSentences = (selectedCorpus: string, query: string, itemsToShow: n
 
   const fetchSentences = async () => {
     try {
-      const response = await fetch(
-        `https://api.wortschatz-leipzig.de/ws/sentences/${selectedCorpus}/sentences/${query}?limit=${itemsToShow}`,
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+      let data: ApiResponseForSentence;
+      if (process.env.NODE_ENV === 'development') {
+        console.log('Using mock data');
+
+        data = mockApiResponse;
+      } else {
+        console.log('Using real data');
+
+        const response = await fetch(
+          `https://api.wortschatz-leipzig.de/ws/sentences/${selectedCorpus}/sentences/${query}?limit=${itemsToShow}`,
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        data = await response.json();
       }
-      const data: ApiResponseForSentence = await response.json();
       setResults(data);
     } catch (error) {
       setError(error.message);
