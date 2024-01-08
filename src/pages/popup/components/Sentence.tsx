6b602450c437dc8ed3e5956c99eb559d758857e2
mshtang <32868278+mshtang@users.noreply.github.com>
2024-01-08 22:41:40 +0100

    feat: add show/hide details functionality to sentence component

--- src/pages/popup/components/Sentence.tsx
+++ src/pages/popup/components/Sentence.tsx
@@ -1,5 +1,6 @@
-import { Link, ListItem, Text } from '@chakra-ui/react';
+import { Box, HStack, IconButton, Link, ListItem, Text } from '@chakra-ui/react';
 import React from 'react';
+import { FaChevronCircleDown, FaChevronCircleUp } from 'react-icons/fa';
 import { Sentence, Source } from '../types/ApiResponseInterfaces';
 
 interface SentenceProps {
@@ -9,26 +10,49 @@ interface SentenceProps {
 }
 
 const Sentence: React.FC<SentenceProps> = ({ sentence, source, queryWord }) => {
+  const [showDetails, setShowDetails] = React.useState(false);
+  const [showChevron, setShowChevron] = React.useState(false);
+
   const urlObject = new URL(source.url);
   const hostname = urlObject.hostname;
   const domain = hostname.replace('www.', '');
 
   return (
     <ListItem p={4} border="1px" borderColor="gray.200" borderRadius="md" mb={4}>
-      <Text>
-        {sentence
-          .split(new RegExp(`(${queryWord})`, 'gi'))
-          .map((part, index) =>
-            part.toLowerCase() === queryWord.toLowerCase() ? <strong key={index}>{part}</strong> : part,
-          )}
-      </Text>
-      <Text mt={2}>
-        Source:{' '}
-        <Link href={source.url} isExternal color="teal.500">
-          {domain}
-        </Link>
-      </Text>
-      <Text mt={2}>Date: {source.date}</Text>
+      <Box position="relative" onMouseEnter={() => setShowChevron(true)} onMouseLeave={() => setShowChevron(false)}>
+        <Text sx={{ minHeight: '24px', textAlign: 'justify' }} fontSize="sm">
+          {sentence
+            .split(new RegExp(`(${queryWord})`, 'gi'))
+            .map((part, index) =>
+              part.toLowerCase() === queryWord.toLowerCase() ? <strong key={index}>{part}</strong> : part,
+            )}
+        </Text>
+        {showDetails && (
+          <HStack sx={{ justifyContent: 'end' }}>
+            <Text mt={2} fontStyle="italic">
+              <strong>Source:</strong>{' '}
+              <Link href={source.url} isExternal color="teal.500">
+                {domain}
+              </Link>
+            </Text>
+            <Text mt={2} fontStyle="italic">
+              <strong>Date:</strong> {source.date}
+            </Text>
+          </HStack>
+        )}
+        {showChevron && (
+          <IconButton
+            position="absolute"
+            right="0"
+            top="0"
+            size={'xs'}
+            alignSelf={'flex-end'}
+            onClick={() => setShowDetails(!showDetails)}
+            icon={showDetails ? <FaChevronCircleUp /> : <FaChevronCircleDown />}
+            aria-label="toggle-details"
+          />
+        )}
+      </Box>
     </ListItem>
   );
 };
